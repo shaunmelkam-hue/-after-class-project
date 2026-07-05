23a0f83d42f7f5d4f821d1e16ad913aa30cbb90f
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-05 18:33:59 -0500

    Add files via upload

---
+++ storing birthday.py
@@ -0,0 +1,15 @@
+
+import datetime
+
+
+lioyd = datetime.date(1980, 2, 19)
+jay = datetime.date(1980, 2, 19)
+kai = datetime.date(1980, 1, 20)
+coel = datetime.date(1980, 1, 31)
+zane = datetime.date(1980, 2, 15)
+
+print("lioyd's birthday is on:", lioyd)
+print("jay's birthday is on:", jay)
+print("kai's birthday is on:", kai)
+print("coel's birthday is on:", coel)
+print("zane's birthday is on:", zane)
