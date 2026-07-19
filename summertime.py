f8b77f53a69e36ad071f2099bf041cb2ef2a25d8
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-07-18 19:41:54 -0500

    Add files via upload

---
+++ summertime.py
@@ -0,0 +1,5 @@
+temperature = int(input("Enter the temperature: "))
+if temperature > 20 -(-80):
+    print("it is not suitable for wearing light clothes")
+else:
+    print("it is suitable for wearing light clothes")
