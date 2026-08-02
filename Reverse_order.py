0f4b8723ca825562104ed2d7f0126070bdbd6ebf
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-08-02 18:56:00 -0500

    Add files via upload

---
+++ Reverse_order.py
@@ -0,0 +1,14 @@
+number = int(input("Enter a number: "))
+
+number = abs(number)
+
+count = 0
+
+if number == 0:
+    count = 1
+else:
+    while number > 0:
+        number // 10
+        count += 1
+
+print("Total number of digits:", count)
