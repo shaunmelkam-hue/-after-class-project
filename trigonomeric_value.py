7d90e975640517cdeb9c552013193c2bc861b373
shaunmelkam-hue <shaunmelkam@gmail.com>
2026-09-12 16:23:38 -0500

    Add files via upload

---
+++ trigonomeric_value.py
@@ -0,0 +1,15 @@
+import math
+angle_degrees = 30
+
+angle_radians = math.radians(angle_degrees)
+
+sine_value = math.sin(angle_radians)
+
+cosine_value = math.cos(angle_radians)
+
+tangent_value = math.tan(angle_radians)
+
+print(f"Angle in Degrees: {angle_degrees}")
+print(f"Sine: {sine_value}")
+print(f"Cosine: {cosine_value}")
+print(f"Tangent: {tangent_value}")
